0ff2742da5556aa5f7632aaaa6f710a141fd7dc7
Bruno Arsioli <57162790+BrunoArsioli@users.noreply.github.com>
2023-03-18 11:01:01 +0000

    Create setup.py
    Create setup.py to make fits-to-parquet into a python packge.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='fits-to-parquet',
+    version='1.0',
+    packages=['fits_to_parquet'],
+    install_requires=[
+        'os'
+        'pandas',
+        'astropy'
+    ],
+    entry_points={
+        'console_scripts': [
+            'fits-to-parquet=fits_to_parquet.fits_to_parquet:main'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
