b5af9506068509019346a0110c3696c988e5e301
Bruno Arsioli <57162790+BrunoArsioli@users.noreply.github.com>
2023-03-18 20:48:05 +0000

    identation again

--- fits_to_parquet/fits_to_parquet.py
+++ fits_to_parquet/fits_to_parquet.py
@@ -1,64 +1,45 @@
-# -*- coding: utf-8 -*-
-"""fits_to_parquet.ipynb
-In case you work in google.colab, run the following cell first:
-
->>>#Allow google.colab to access files in your drive
->>>from google.colab import drive
->>>drive.mount('/content/drive', force_remount=True)
-"""
-
-
-# import libraries
-import os
-import pandas 
-import astropy
-from astropy.table import Table
-
-# read .fits write .parquet
 def fits_to_parquet(fits_path):
-   """
+    """
     Converts a FITS file to a parquet file.
-
     Parameters
     ----------
     fits_path : str
         Path to the FITS file.
-
     Returns
     -------
     str
         Path to the parquet file.
-   """
+    """
 
-  # Check that the input file exists 
-  if not os.path.isfile(fits_path):
-      raise ValueError("Input file does not exist")
+    # Check that the input file exists 
+    if not os.path.isfile(fits_path):
+        raise ValueError("Input file does not exist")
 
 
-  # read .fits file 
-  try:
-      data = Table.read(f"{fits_path}")
-  except ValueError:
-      raise ValueError("Input file is not a valid .fits file.")  
-  
-  # help user deal with exception in case input file is not .fits neither .fit file.
-  if not fits_path.endswith('.fits') and not fits_path.endswith('.fit'):
-      raise ValueError('Input file has wrong extension. Must be .fits or .fit')
+    # read .fits file 
+    try:
+        data = Table.read(f"{fits_path}")
+    except ValueError:
+        raise ValueError("Input file is not a valid .fits file.")  
 
-  # .fits to pandas dataframe
-  datadf = data.to_pandas()
+    # help user deal with exception in case input file is not .fits neither .fit file.
+    if not fits_path.endswith('.fits') and not fits_path.endswith('.fit'):
+        raise ValueError('Input file has wrong extension. Must be .fits or .fit')
 
-  # remove extention from fits_path
-  if fits_path.endswith('.fits'):
-      parquet_path = fits_path.replace(".fits", ".parquet")
-  elif:
-      parquet_path = fits_path.replace(".fit", ".parquet")
+    # .fits to pandas dataframe
+    datadf = data.to_pandas()
 
-  # check if .parquet file already exist
-  if os.path.isfile(parquet_path):
-      raise ValueError("Output file already exists") 
+    # remove extention from fits_path
+    if fits_path.endswith('.fits'):
+        parquet_path = fits_path.replace(".fits", ".parquet")
+    elif:
+        parquet_path = fits_path.replace(".fit", ".parquet")
 
-  # df to .parquet
-  datadf.to_parquet(f"{parquet_path}")
+    # check if .parquet file already exist
+    if os.path.isfile(parquet_path):
+        raise ValueError("Output file already exists") 
 
-  return print(f".fits to .parquet is done! \n.parquet saved at: {parquet_path}")
+    # df to .parquet
+    datadf.to_parquet(f"{parquet_path}")
+
+    return print(f".fits to .parquet is done! \n.parquet saved at: {parquet_path}")
