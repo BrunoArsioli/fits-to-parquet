e758570a86adf5e739b216ae5ffcdbe06a1de0b5
Bruno Arsioli <57162790+BrunoArsioli@users.noreply.github.com>
2023-03-18 20:35:41 +0000

    Update fits_to_parquet.py

--- fits_to_parquet/fits_to_parquet.py
+++ fits_to_parquet/fits_to_parquet.py
@@ -43,20 +43,20 @@ def fits_to_parquet(fits_path):
   
   # help user deal with exception in case input file is not .fits neither .fit file.
   if not fits_path.endswith('.fits') and not fits_path.endswith('.fit'):
-   raise ValueError('Input file has wrong extension. Must be .fits or .fit')
+    raise ValueError('Input file has wrong extension. Must be .fits or .fit')
 
   # .fits to pandas dataframe
   datadf = data.to_pandas()
 
   # remove extention from fits_path
   if fits_path.endswith('.fits'):
-   parquet_path = fits_path.replace(".fits", ".parquet")
+    parquet_path = fits_path.replace(".fits", ".parquet")
   elif:
-   parquet_path = fits_path.replace(".fit", ".parquet")
+    parquet_path = fits_path.replace(".fit", ".parquet")
 
   # check if .parquet file already exist
   if os.path.isfile(parquet_path):
-   raise ValueError("Output file already exists") 
+    raise ValueError("Output file already exists") 
 
   # df to .parquet
   datadf.to_parquet(f"{parquet_path}")
